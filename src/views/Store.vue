85b385be37848ec9fd47cf4ed01a06864f6977b4
Samuel <samueljonascali@gmail.com>
2025-04-07 17:41:38 -0300

    update store view

---
+++ src/views/Store.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input
+      v-model="filters.name"
+      placeholder="Nome do Pokemon"
+      class="form-control mb-2"
+    />
+    <input
+      v-model="filters.id"
+      placeholder="ID do Pokemon"
+      type="number"
+      class="form-contrrol"
+      mb-2
+    />
+    <input
+      v-model="filters.type"
+      placeholder="Tipo "
+      class="form-control mb-2"
+    />
+    <input
+      v-model="filters.species"
+      placeholder="Espécie"
+      class="form-control mb-2"
+    />
+    <ul>
+      <li v-for="pokemon in filteredPokemons" :key="pokemon.id">
+        {{ pokemon.name }} (ID: {{ pokemon.id }})
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { mapState, mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapState(["filters"]),
+    ...mapGetters(["filteredPokemons"]),
+  },
+};
+</script>
